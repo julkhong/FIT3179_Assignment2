555a932b4d263c97693a609c50bc415cd6b71bde
Julian <70477671+julkhong@users.noreply.github.com>
2021-10-18 05:07:40 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,36 @@
+var vg_1 = "covid.json";
+vegaEmbed("#covid_map", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+var vg_2 = "radar_full.json";
+vegaEmbed("#radar_chart", vg_2).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+var vg_3 = "scatterplot.json";
+vegaEmbed("#scatterplot", vg_3).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+var vg_4 = "map2.json";
+vegaEmbed("#flow_map", vg_4).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+var vg_5 = "radar1.json";
+vegaEmbed("#radar1", vg_5).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+var vg_6 = "radar2.json";
+vegaEmbed("#radar2", vg_6).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+var vg_7 = "areachart.json";
+vegaEmbed("#areachart", vg_7).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+var vg_8 = "areachart2.json";
+vegaEmbed("#areachart2", vg_8).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+var vg_9 = "areachart3.json";
+vegaEmbed("#areachart3", vg_9).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
